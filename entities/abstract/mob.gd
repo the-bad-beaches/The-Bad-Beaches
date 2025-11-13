723d7669132267ddb99c49b91a3e10e8a8ffa118
Christopher De Jesus <christopher2653@gmail.com>
2025-11-12 20:05:50 -0500

    Rudimentary HUD and mob pathing.

--- entities/abstract/mob.gd
+++ entities/abstract/mob.gd
@@ -1,5 +1,9 @@
 extends RigidBody2D
 
+@export var mob_path: PathFollow2D
+
+var percent_goal = 0.0
+var rate = 0.01
 
 func _ready() -> void:
 	# Choose a random sprite and play it
@@ -7,8 +11,17 @@ func _ready() -> void:
 		$AnimatedSprite2D.sprite_frames.get_animation_names()
 	).pick_random()
 	$AnimatedSprite2D.play()
+	if percent_goal == null:
+		percent_goal = 0.0
 
+func _process(delta) -> void:
+	percent_goal = clamp(percent_goal + 0.01 * delta, 0.0, 1.0)
+	progress_path()
 
 # Free node when off screen
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
+
+func progress_path() -> void:
+	mob_path.progress_ratio = percent_goal
+	position = mob_path.position
--- levels/level_1.gd
+++ levels/level_1.gd
@@ -9,23 +9,20 @@ func _ready() -> void:
 	$StartTimer.start()
 
 
-
-
 func _on_spawn_timer_timeout() -> void:
 	# Spawn mob
 	var mob = mob_scene.instantiate()
-	
-	var mob_spawn_loc = $MobPath/MobSpawnLocation
-	mob_spawn_loc.progress_ratio = randf()
-	
+	var mob_spawn_loc = $MobPath/MobFollowPath
+	mob_spawn_loc.progress_ratio = 0
+	mob.mob_path = mob_spawn_loc
 	mob.position = mob_spawn_loc.position
 	
 	#var direction = mob_spawn_loc.rotation + PI / 2
 	#direction += randf_range(-PI / 4, PI / 4)
 	#mob.rotation = direction	
 	
-	var vel = Vector2(randf_range(150.0, 250.0), 0.0)
-	mob.linear_velocity = vel
+	#var vel = Vector2(randf_range(150.0, 250.0), 0.0)
+	#mob.linear_velocity = vel
 	
 	add_child(mob)
 
